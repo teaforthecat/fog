54ba94a8f5d6b542540f5a7200b90b74d18d685e
Chris Thompson <chris.thompson@govdelivery.com>
2014-08-20 10:01:37 -0500

    [vsphere] find network by name and dvswitch
    given a name and a switch name
    or just a name and whether a dvs is desired
    or just a name

--- lib/fog/vsphere/requests/compute/get_network.rb
+++ lib/fog/vsphere/requests/compute/get_network.rb
@@ -11,14 +11,43 @@ module Fog
         protected
 
         def get_raw_network(name, datacenter_name, distributedswitch_name=nil)
-          dc = find_raw_datacenter(datacenter_name)
-
-          @connection.serviceContent.viewManager.CreateContainerView({
-            :container  => dc.networkFolder,
-            :type       =>  ["Network"],
-            :recursive  => true
-          }).view.select { |n| n.name == name and (not distributedswitch_name or n.config.distributedVirtualSwitch.name == distributedswitch_name)}.first
+          finder = choose_finder(name, distributedswitch_name)
+          networks = get_all_raw_networks(datacenter_name)
+          networks.find{ |n| finder.(n) }
         end
+      end
+
+      module Shared
+
+        protected
+
+        def get_all_raw_networks datacenter_name
+          dc = find_raw_datacenter(datacenter_name)
+          @connection.serviceContent.viewManager.
+            CreateContainerView({
+                                 container: dc.networkFolder,
+                                 type:      ["Network"],
+                                 recursive: true
+                                }).view
+        end
+
+        def choose_finder name, distributedswitch_name
+          case distributedswitch_name
+          when String
+            # only the one will do
+            ->(n){ (n.name == name ) &&
+                (n.class.to_s == "DistributedVirtualPortgroup") &&
+                (n.config.distributedVirtualSwitch.name == distributedswitch_name)
+            }
+          when true
+            # the first distributed virtual switch will do - selected by network - gives control to vsphere
+            ->(n){ n.name == name && n.class.to_s == "DistributedVirtualPortgroup"}
+          else
+            # the first matching network will do, seems like the non-distributed networks come first
+            ->(n){ n.name == name }
+          end
+        end
+
       end
 
       class Mock
